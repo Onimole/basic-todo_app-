443ba2e26526c3eeda0312efbd3139d33494c8d2
unknown <onimoletaofeek@gmail.com>
2023-04-13 18:57:38 +0100

    Basic To-do app

--- Todoo/settings.py
+++ Todoo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1rm4dbgl&)k8&305bealf@l26*rws0ol51u3ddy+)te7d018c!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
